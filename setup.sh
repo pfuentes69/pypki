e124d27d76fa30c5aa2db23c8f918354b43d79d8
pfuentes69 <pedro.fuentes@naviter.es>
2026-03-19 22:24:40 +0900

    Fix setup

--- setup.sh
+++ setup.sh
@@ -142,6 +142,7 @@ mariadb --batch -u root <<SQL
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`
     CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
+ALTER USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
 SQL
